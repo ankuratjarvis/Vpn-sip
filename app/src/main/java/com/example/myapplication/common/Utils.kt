214e00a89649a3bfedd25348ff45a97735c8928c
spx-jc <sarthak.pandey@jarvis.consulting>
2023-09-12 16:22:19 +0530

    sip with call active screen added

--- app/src/main/java/com/example/myapplication/common/Utils.kt
+++ app/src/main/java/com/example/myapplication/common/Utils.kt
@@ -1,12 +1,22 @@
 package com.example.myapplication.common
 
+import android.app.Activity
+import android.app.ActivityManager
+import android.app.ActivityManager.RunningAppProcessInfo
+import android.app.AppComponentFactory
 import android.content.Context
 import android.net.ConnectivityManager
 import android.util.Log
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.example.myapplication.MainActivity
+import com.example.myapplication.R
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.TimeZone
+
 
 fun TextView.DisplayLogs(logmsg: String) {
     var logmsg = logmsg
@@ -34,3 +44,20 @@ fun netCheck(context: Context): Boolean {
     val nInfo = cm.activeNetworkInfo
     return nInfo != null && nInfo.isConnectedOrConnecting
 }
+
+fun isAppInForeground(context: Context): Boolean {
+    val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+    // Get the list of running processes
+    val processes = activityManager.runningAppProcesses
+    if (processes != null) {
+        for (processInfo in processes) {
+            if (processInfo.processName == context.packageName) {
+                // Check if the process importance is foreground
+                return processInfo.importance == RunningAppProcessInfo.IMPORTANCE_FOREGROUND
+            }
+        }
+    }
+    return false
+}
+
+
